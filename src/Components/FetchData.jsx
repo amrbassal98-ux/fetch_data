617fa0f39b51c9b9bd3b5fd5e0fbc9639cea65d7
Amr Bassal <amrbassal98@gmail.com>
2025-11-17 13:59:29 +0200

    created customer hook and implemented it to fetch data using APIs easily

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import UseFetch from './UseFetch';
+import './FetchData.css';
 const FetchData = () => {
+  const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+  console.log(data);
   return (
     <>
+      <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
      <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+        {data && data.map((e, index) =>(
+          <li key={index} className='list_data'>
+            <img src={e.image} alt={e.name} className='images'/>
+            <h3>{e.name}</h3>
+            <p><strong>Importance: </strong>{e.importance}</p>
+            <p><strong>Benefits: </strong>{e.benefits}</p>
+            <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+          </li>
+        ))}
      </ul>
     </>
-  )
+  );
 }
 
 export default FetchData
